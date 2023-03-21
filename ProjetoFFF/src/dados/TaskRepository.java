870b847216960a7b12de8ef87cf57204fe9e42e5
ilana <ilanabrochardtps@gmail.com>
2023-03-20 21:17:22 -0300

    Criação da classe task e task repositório

--- ProjetoFFF/src/dados/TaskRepository.java
+++ ProjetoFFF/src/dados/TaskRepository.java
@@ -1,6 +1,38 @@
 package dados;
 
-public class TaskRepository implements IRepository {
+import exceptions.*;
 
+import java.util.List;
+
+public class TaskRepository implements IRepository {
+    @Override
+    public List listarTodos() {
+        return null;
+    }
+
+    @Override
+    public Object listarPorId(int id) throws ElementoNaoEncontradoException, ArgumentoInvalidoException {
+        return null;
+    }
+
+    @Override
+    public Object listarPorNome(String nome) throws ElementoNaoEncontradoException, ArgumentoInvalidoException {
+        return null;
+    }
+
+    @Override
+    public void adicionar(Object a) throws ElementoJaExisteException, ArgumentoInvalidoException {
+
+    }
+
+    @Override
+    public void atualizar(Object item) throws AtualizacaoFalhouException, ElementoNaoEncontradoException, ArgumentoInvalidoException {
+
+    }
+
+    @Override
+    public void remover(Object a) throws DeletarFalhouException, ElementoNaoEncontradoException, ArgumentoInvalidoException {
+
+    }
 }
 
--- ProjetoFFF/src/negocio/beans/Task.java
+++ ProjetoFFF/src/negocio/beans/Task.java
@@ -1,4 +1,74 @@
 package negocio.beans;
 
+import enums.Prioridades;
+import enums.Status;
+
+import java.time.LocalDate;
+
 public class Task {
+    private String titulo;
+    private String conteudo;
+    private Status status;
+    private LocalDate dataCriada;
+    private Prioridades prioridades;
+    private List<Pomodoro> pomodoros;
+
+    public Task(String titulo, String conteudo, Status status, LocalDate dataCriada, Prioridades prioridades, List<Pomodoro> pomodoros) {
+        this.titulo = titulo;
+        this.conteudo = conteudo;
+        this.status = status;
+        this.dataCriada = dataCriada;
+        this.prioridades = prioridades;
+        this.pomodoros = pomodoros;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public LocalDate getDataCriada() {
+        return dataCriada;
+    }
+
+    public void setDataCriada(LocalDate dataCriada) {
+        this.dataCriada = dataCriada;
+    }
+
+    public Prioridades getPrioridades() {
+        return prioridades;
+    }
+
+    public void setPrioridades(Prioridades prioridades) {
+        this.prioridades = prioridades;
+    }
+
+    public List<Pomodoro> getPomodoros() {
+        return pomodoros;
+    }
+
+    public void setPomodoros(List<Pomodoro> pomodoros) {
+        this.pomodoros = pomodoros;
+    }
+
+
 }
