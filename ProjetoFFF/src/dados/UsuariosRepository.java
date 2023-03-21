1a90563cc912d689a84ba27c92b62ee3befdf125
ilana <ilanabrochardtps@gmail.com>
2023-03-21 17:25:35 -0300

    modificando no usuarioRepository

--- ProjetoFFF/src/dados/UsuariosRepository.java
+++ ProjetoFFF/src/dados/UsuariosRepository.java
@@ -6,16 +6,16 @@ import negocio.beans.Usuario;
 import java.util.ArrayList;
 import java.util.List;
 
-public class UsuariosRepository implements IRepository<UsuariosRepository> {
+public class UsuariosRepository implements IRepository<Usuario> {
     List<Usuario> usuarios = new ArrayList<>();
 
     @Override
-    public List<UsuariosRepository> listarTodos() {
-        return null;
+    public List<Usuario> listarTodos() {
+        return usuarios;
     }
 
     @Override
-    public UsuariosRepository listarPorId(int id) throws ElementoNaoEncontradoException, ArgumentoInvalidoException {
+    public Usuario listarPorId(int id) throws ElementoNaoEncontradoException, ArgumentoInvalidoException {
         return null;
     }
 
@@ -25,17 +25,12 @@ public class UsuariosRepository implements IRepository<UsuariosRepository> {
     }
 
     @Override
-    public void adicionar(UsuariosRepository item) throws ElementoJaExisteException, ArgumentoInvalidoException {
+    public void atualizar(Usuario item) throws AtualizacaoFalhouException, ElementoNaoEncontradoException, ArgumentoInvalidoException {
 
     }
 
     @Override
-    public void atualizar(UsuariosRepository item) throws AtualizacaoFalhouException, ElementoNaoEncontradoException, ArgumentoInvalidoException {
-
-    }
-
-    @Override
-    public void remover(UsuariosRepository item) throws DeletarFalhouException, ElementoNaoEncontradoException, ArgumentoInvalidoException {
+    public void remover(Usuario item) throws DeletarFalhouException, ElementoNaoEncontradoException, ArgumentoInvalidoException {
 
     }
 }
