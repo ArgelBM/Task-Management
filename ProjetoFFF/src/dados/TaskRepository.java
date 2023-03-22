ec533b9e76081ff38a5394f520f81d04d25175da
ilana <ilanabrochardtps@gmail.com>
2023-03-21 22:00:08 -0300

    acrescentando os metodos no Repository, iserindo o enum filtro e acrescentando em controlador

--- ProjetoFFF/src/dados/TaskRepository.java
+++ ProjetoFFF/src/dados/TaskRepository.java
@@ -1,8 +1,12 @@
 package dados;
 
+import enums.Prioridades;
+import enums.Status;
 import exceptions.*;
 import negocio.beans.Task;
+import negocio.beans.Usuario;
 
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,6 +38,7 @@ public class TaskRepository implements IRepository<Task> {
         throw new ElementoNaoEncontradoException(nome);
     }
 
+    @Override
     public static void adicionar(Task item) throws ElementoJaExisteException, ArgumentoInvalidoException {
         if (item == null){
             throw new ArgumentoInvalidoException(item);
@@ -58,12 +63,115 @@ public class TaskRepository implements IRepository<Task> {
 
     @Override
     public void atualizar(Task item) throws AtualizacaoFalhouException, ElementoNaoEncontradoException, ArgumentoInvalidoException {
-
+        if (item == null) {
+            throw new ArgumentoInvalidoException(null);
+        }
+        if (item.getNome() == null || item.getNome().trim().isEmpty()) {
+            throw new ArgumentoInvalidoException(item);
+        }
+        if (item.getConteudo() == null || item.getConteudo().trim().isEmpty()) {
+            throw new ArgumentoInvalidoException(item);
+        }
+        if (item.getStatus() == null) {
+            throw new ArgumentoInvalidoException(item);
+        }
+        if (item.getPrioridades() == null) {
+            throw new ArgumentoInvalidoException(item);
+        }
+        int index = listasDeTask.indexOf(item);
+        if (index == -1) {
+            throw new ElementoNaoEncontradoException(item);
+        }
+        Task taskAntiga = listasDeTask.get(index);
+        taskAntiga.setNome(item.getNome());
+        taskAntiga.setConteudo(item.getConteudo());
+        taskAntiga.setStatus(item.getStatus());
+        taskAntiga.setPrioridades(item.getPrioridades());
     }
 
     @Override
     public void remover(Task item) throws DeletarFalhouException, ElementoNaoEncontradoException, ArgumentoInvalidoException {
-
+        if (item == null) {
+            throw new ArgumentoInvalidoException(null);
+        }
+        int index = listasDeTask.indexOf(item);
+        if (index == -1) {
+            throw new ElementoNaoEncontradoException(item);
+        }
+        if (!listasDeTask.remove(item)) {
+            throw new DeletarFalhouException(item);
+        }
     }
+
+    public List<Task> listarPorStatus(Status status) throws ElementoNaoEncontradoException {
+        List<Task> tasksComStatus = new ArrayList<Task>();
+
+        for(Task task : listasDeTask){
+            if(task.getStatus() == status){
+                tasksComStatus.add(task);
+            }
+        }
+
+        if(tasksComStatus.isEmpty()){
+            throw new ElementoNaoEncontradoException("Nenhuma tarefa encontrada com o status " + status);
+        }
+
+        return tasksComStatus;
+    }
+
+    public List<Task> listarPorPrioridade(Prioridades prioridade) throws ElementoNaoEncontradoException {
+        List<Task> tasksComPrioridade = new ArrayList<Task>();
+
+        for(Task task : listasDeTask){
+            if(task.getPrioridades() == prioridade){
+                tasksComPrioridade.add(task);
+            }
+        }
+
+        if(tasksComPrioridade.isEmpty()){
+            throw new ElementoNaoEncontradoException("Nenhuma tarefa encontrada com a prioridade " + prioridade);
+        }
+
+        return tasksComPrioridade;
+    }
+
+    public List<Task> listarPorCor(String cor) throws ElementoNaoEncontradoException {
+        List<Task> tasksComCor = new ArrayList<Task>();
+
+        for(Task task : listasDeTask){
+            if(task.getCor().equals(cor)){
+                tasksComCor.add(task);
+            }
+        }
+
+        if(tasksComCor.isEmpty()){
+            throw new ElementoNaoEncontradoException("Nenhuma tarefa encontrada com a cor " + cor);
+        }
+
+        return tasksComCor;
+    }
+
+    public List<Task> listarPorUsuario(String usuario) throws ElementoNaoEncontradoException {
+        List<Task> tasksDoUsuario = new ArrayList<Task>();
+
+        for(Task task : listasDeTask){
+            if(task.getUsuario().equals(usuario)){
+                tasksDoUsuario.add(task);
+            }
+        }
+
+        if(tasksDoUsuario.isEmpty()){
+            throw new ElementoNaoEncontradoException("Nenhuma tarefa encontrada para o usuário " + usuario);
+        }
+
+        return tasksDoUsuario;
+    }
+
+
+    public void gerarRelatorioPorMes(Month){ }
+
+    public void  salvarTarefas(){ }
+
+    public void carregarTarefas(){ }
 }
 
