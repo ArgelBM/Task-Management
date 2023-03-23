3e0689675dc0351ea30607e82824c30435dbd123
Argel <arbezerra14@outlook.com>
2023-03-22 22:47:03 -0300

    att

--- ProjetoFFF/src/negocio/ControladorCategorias.java
+++ ProjetoFFF/src/negocio/ControladorCategorias.java
@@ -1,5 +1,10 @@
 package negocio;
 
+import dados.CategoriasRepository;
+import exceptions.ArgumentoInvalidoException;
+import exceptions.DeletarFalhouException;
+import exceptions.ElementoJaExisteException;
+import exceptions.ElementoNaoEncontradoException;
 import negocio.beans.Categoria;
 
 import java.util.List;
@@ -14,15 +19,23 @@ public class ControladorCategorias {
         return instance;
     }
 
-    public static void adicionar(Categoria obj) {
+    public static void adicionar(Categoria obj) throws ElementoJaExisteException, ArgumentoInvalidoException {
+        CategoriasRepository repo = new CategoriasRepository();
+        repo.adicionar(obj);
     }
 
-    public static void remover(Categoria obj) {
+    public static void remover(Categoria obj) throws DeletarFalhouException, ElementoNaoEncontradoException, ArgumentoInvalidoException {
+        CategoriasRepository repo = new CategoriasRepository();
+        repo.remover(obj);
     }
 
     public static List<Categoria> listarPorTodos() {
+        CategoriasRepository repo = new CategoriasRepository();
+        return repo.listarTodos();
     }
 
-    public static List<Categoria> listarPornome(String nome) {
+    public static Categoria listarPornome(String nome)  throws ElementoNaoEncontradoException, ArgumentoInvalidoException {
+        CategoriasRepository repo = new CategoriasRepository();
+        return repo.listarPorNome(nome);
     }
 }
