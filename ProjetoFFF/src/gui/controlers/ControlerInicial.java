42907a9bb321938120ba86336ea909c80d1ee2b8
Joao Lucas <joaolucas.santana@outlook.com>
2023-04-06 17:25:32 -0300

    ControlerInicial

--- ProjetoFFF/src/gui/controlers/ControlerInicial.java
+++ ProjetoFFF/src/gui/controlers/ControlerInicial.java
@@ -4,38 +4,42 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.BorderPane;
 
+
+import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ControlerInicial implements Initializable {
 
-    private StackPane contentArea;
+    @FXML
+    private BorderPane contentArea;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         try {
-            Parent fxml = FXMLLoader.load(getClass().getResource("telas/TelaLogin.fxml"));
-
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
+            trocarConteudoDireita("/gui/telas/TelaLogin.fxml");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    @FXML
-    private void TelaLogin(javafx.event.ActionEvent event) throws IOException{
-        Parent fxml = FXMLLoader.load(getClass().getResource("telas/TelaLogin.fxml"));
-        contentArea.getChildren().removeAll();
-        contentArea.getChildren().setAll(fxml);
+    private void trocarConteudoDireita(String caminhoFXML) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(caminhoFXML));
+        Parent root = loader.load();
+        contentArea.setRight(root);
     }
 
     @FXML
-    private void TelaCadastro(javafx.event.ActionEvent event) throws IOException{
-        Parent fxml = FXMLLoader.load(getClass().getResource("telas/TelaCadastro.fxml"));
-        contentArea.getChildren().removeAll();
-        contentArea.getChildren().setAll(fxml);
+    private void exibirTelaLogin(ActionEvent event) throws IOException {
+        trocarConteudoDireita("/gui/telas/TelaLogin.fxml");
+    }
+
+    @FXML
+    private void exibirTelaCadastro(ActionEvent event) throws IOException {
+        trocarConteudoDireita("/gui/telas/TelaCadastro.fxml");
     }
 
 }
