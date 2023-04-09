d718d25f45259c9525024cb095b15ef0ab73fc9c
Joao Lucas <joaolucas.santana@outlook.com>
2023-04-09 19:54:21 -0300

    Hoje itens

--- ProjetoFFF/src/gui/controlers/ControlerHoje.java
+++ ProjetoFFF/src/gui/controlers/ControlerHoje.java
@@ -1,4 +1,52 @@
 package gui.controlers;
 
-public class ControlerHoje {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControlerHoje implements Initializable {
+    @FXML
+    private VBox tarefas;
+
+    @FXML
+    private TextField novaTarefa;
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        carregarItens();
+    }
+
+    private void carregarItens() {
+        tarefas.getChildren().clear();
+        for (int i = 0; i < 10; i++) {
+            try {
+                Node item = FXMLLoader.load(getClass().getResource("/gui/telas/Item.fxml"));
+                tarefas.getChildren().add(item);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @FXML
+    void adicionarTarefa() {
+        if (!novaTarefa.getText().isEmpty()) {
+            try {
+                Node item = FXMLLoader.load(getClass().getResource("/gui/telas/Item.fxml"));
+                tarefas.getChildren().add(item);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            novaTarefa.clear();
+        }
+    }
 }
